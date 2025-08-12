2b0b286d36f431349ecf0399389f9dc4cb523dda
noClaps <git@zerolimits.dev>
2025-08-12 11:50:14 +0100

    cli: Skip logging if package is already up to date

--- internal/cmd/update.go
+++ internal/cmd/update.go
@@ -16,7 +16,6 @@ func Update() {
 	for _, pkg := range pkgs {
 		pkgManifest := manifest.GetManifest(pkg)
 		if pkgManifest.Version == lockfile.Packages[pkg].Version {
-			fmt.Println(pkg + " is already up to date")
 			continue
 		}
 
