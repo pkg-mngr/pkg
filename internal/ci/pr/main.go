060b94190c5423141bcc456a26ee5dc23097d53b
noClaps <git@zerolimits.dev>
2025-08-18 14:39:28 +0100

    packages: Add Erlang (#21)
    * packages: Add Erlang
    
    * ci: Initialise pkg in PR
    
    * packages: Add slow install caveat to Erlang

--- internal/ci/pr/main.go
+++ internal/ci/pr/main.go
@@ -4,11 +4,13 @@ import (
 	"os"
 
 	"github.com/noclaps/pkg/internal/cmd"
+	"github.com/noclaps/pkg/internal/config"
 	"github.com/noclaps/pkg/internal/log"
 )
 
 func main() {
 	files := os.Args[1:]
+	config.Init()
 
 	for _, file := range files {
 		log.Println("Checking if installation works...")
