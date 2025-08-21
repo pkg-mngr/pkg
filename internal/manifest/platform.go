75ef15b4fda51ac95c8c924a79aa65ae74de1e8f
noClaps <git@zerolimits.dev>
2025-08-21 21:20:07 +0100

    cli: Support new manifest format

---
+++ internal/manifest/platform.go
@@ -0,0 +1,21 @@
+package manifest
+
+import (
+	"fmt"
+	"runtime"
+)
+
+type Platform string
+
+func GetPlatform() Platform {
+	os := runtime.GOOS
+	arch := runtime.GOARCH
+
+	if os == "darwin" {
+		os = "macos"
+	}
+	if arch == "amd64" {
+		arch = "x64"
+	}
+	return Platform(fmt.Sprintf("%s-%s", os, arch))
+}
