aba815bba6ba27a76b84b5b0af24c33dc81a9b55
noClaps <git@zerolimits.dev>
2025-08-23 20:15:43 +0100

    cli: Simplify utils.RunScript() function

--- internal/util/run_script.go
+++ internal/util/run_script.go
@@ -2,12 +2,12 @@ package util
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/pkg-mngr/pkg/internal/config"
+	"github.com/pkg-mngr/pkg/internal/log"
 )
 
 func RunScript(script string, skipConfirmation bool) (string, error) {
@@ -22,32 +22,14 @@ func RunScript(script string, skipConfirmation bool) (string, error) {
 	script = fmt.Sprintf("set -euo pipefail\ncd %s\n%s", config.PKG_TMP, script)
 	cmd := exec.Command(shell, "-c", script)
 
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		return "", fmt.Errorf("Error getting stderr pipe: %v", err)
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		return "", fmt.Errorf("Error getting stderr pipe: %v", err)
-	}
-	if err := cmd.Start(); err != nil {
-		return "", fmt.Errorf("Error while starting command: %v", err)
-	}
+	var stdout, stderr strings.Builder
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
-	stdoutData, err := io.ReadAll(stdout)
-	if err != nil {
-		return "", fmt.Errorf("Error getting data from stderr: %v", err)
+	if err := cmd.Run(); err != nil {
+		log.Errorf("Error running command: %v", err)
 	}
-	stderrData, err := io.ReadAll(stderr)
-	if err != nil {
-		return "", fmt.Errorf("Error getting data from stderr: %v", err)
-	}
-
-	if err := cmd.Wait(); err != nil {
-		return "", fmt.Errorf("Error waiting for command: %v\nstdout:\n%s\nstderr:\n%s", err, stdoutData, stderrData)
-	}
-
-	return string(stdoutData), fmt.Errorf("%s", stderrData)
+	return stdout.String(), fmt.Errorf("%s", &stderr)
 }
 
 func getConfirmation(script string) bool {
